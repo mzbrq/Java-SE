ff75138b0c9be76ecfcbb6e1a3ca5e7f6f958ba3
mzbrq <1404461081@qq.com>
2025-03-01 14:26:42 +0800

    code

---
+++ test_2_27/src/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    public static void main(String[] args) {
+
+    }
+    public static void main1(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        //数组对象.length遍历数组
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }*/
+
+        //for - each
+        /*for (int x : arr) {
+            System.out.println(x + "");
+        }*/
+    }
+}
